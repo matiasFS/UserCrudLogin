6817411756c872ced4acb0fa96604783e8993e7d
Matias Silvestri <71100012+matiasFS@users.noreply.github.com>
2024-07-10 19:05:54 -0300

    Inicio aplicacion backend

---
+++ UserCrudLoginBackend/src/main/java/com/matiasfs/usercrudlogin/UsercrudloginApplication.java
@@ -0,0 +1,13 @@
+package com.matiasfs.usercrudlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercrudloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercrudloginApplication.class, args);
+	}
+
+}
---
+++ UserCrudLoginBackend/src/test/java/com/matiasfs/usercrudlogin/UsercrudloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasfs.usercrudlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercrudloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
